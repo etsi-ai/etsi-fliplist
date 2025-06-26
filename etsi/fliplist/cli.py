6a35248e2d9063c50629b93efc690f4863894447
Priyansh Srivastava <124620152+PriyanshSrivastava0305@users.noreply.github.com>
2025-06-26 10:25:42 +0530

    v0.1.1

--- etsi/fliplist/cli.py
+++ etsi/fliplist/cli.py
@@ -6,7 +6,7 @@ from .utils import print_banner
 
 
 def main():
-    print_banner()
+    # print_banner()
     parser = argparse.ArgumentParser(description="Flip a list of items")
     parser.add_argument("items", nargs='+', help="Items to reverse")
     args = parser.parse_args()
